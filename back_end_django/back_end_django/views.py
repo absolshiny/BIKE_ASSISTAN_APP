5f2a07cf05e01ac425969b3f076c97fb5e6f0ee4
DSegura <absol.1994@gmail.com>
2023-10-03 00:33:45 -0600

    changed

--- back_end_django/back_end_django/views.py
+++ back_end_django/back_end_django/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-import openai, os
-from dataenv import load_dotenv
-load_dotenv()
 
-api_key=os.getenv("OPENAI_KEY", None)
 
-def chetbot
+api_key="asdasfasfasda"
+
+def chatbot(request):
+    user_input= request.POST.get("user_input")
+    print(user_input)
+    return render(request, "main.html", {})
